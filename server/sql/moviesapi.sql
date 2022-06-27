92ce9134a44318339ad279ef28212fb2fc293372
simelane-jpd <201575227@student.uj.ac.za>
2022-06-27 15:13:26 +0200

    playlist database

--- server/sql/moviesapi.sql
+++ server/sql/moviesapi.sql
@@ -1,6 +1,18 @@
 create table users(
 	id serial not null primary key,
+	firstname text,
+	lastname text,
 	username text,
 	password text,
 	
 );
+
+create table playlist (
+	playlistid serial not null primary key,
+	personsid int,
+	moviename text,
+	foreign key (personsid) references users(id)
+	
+	
+	
+)
